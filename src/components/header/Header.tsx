92464fe4d9625162ceb8d821f60b3dfa1efcacc8
ValentainFomkin <valentain1625@mail.ru>
2023-02-04 04:39:08 +0300

    created header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <img
+                src="https://cdn.dribbble.com/users/153055/screenshots/14524552/media/1c289b4d79870a1bbd71ac00b429e671.jpg?compress=1&resize=400x300&vertical=top"
+                alt="balerina-logo"/>
+        </div>
+    );
+};
+
