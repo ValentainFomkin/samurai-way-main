827626ae185cafef570866e9902490ef73528300
ValentainFomkin <valentain1625@mail.ru>
2023-02-06 21:13:00 +0300

    changes component

--- src/render.tsx
+++ src/render.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-import {addPost, StateType} from "./redux/state";
+import {addPost, StateType, updateNewPostText} from "./redux/state";
 import {BrowserRouter} from 'react-router-dom';
 
 
@@ -12,6 +12,7 @@ export let renderEntireTree = (props: StateType) => {
             <App
                 state={props}
                 addPost={addPost}
+                updateNewPostText={updateNewPostText}
             />,
         </BrowserRouter>, document.getElementById('root')
     )
