9a757d905cdfee3d28752121ccf2a5480c65fa3f
ValentainFomkin <valentain1625@mail.ru>
2023-02-06 20:26:07 +0300

    changes component

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,32 +1,53 @@
 import {v1} from "uuid";
+import {renderEntireTree} from "../render";
 
+export type StateType = {
+    postsData: PostDataType
+    dialogsData: DialogsDataType
+    navigationData: NavigationDataType
+}
 export type PostDataType = {
+    post: PostType[]
+
+}
+export type PostType = {
     id: string
-    postsTitle: string
     postMessage: string
     likesCount: number
 }
+
+
 export type DialogsDataType = {
-    id: string
-    name: string
+    dialogs: DialogsType[]
+    messages: MessagesType[]
 }
 export type MessagesType = {
     id: string
     message: string
 }
-export type NameFriendsType = {
+export type DialogsType = {
     id: string
     name: string
 }
 
-export const state = {
+
+export type NavigationDataType = {
+    nameFriend: NameFriendType[]
+}
+export type NameFriendType = {
+    id: string
+    name: string
+}
+
+
+export const state: StateType = {
     postsData: {
         post: [
-            {id: v1(), postsTitle: 'Dima', postMessage: `i'm learning React!`, likesCount: 10},
-            {id: v1(), postsTitle: 'Valya', postMessage: `i'm learning Redux!`, likesCount: 10},
-            {id: v1(), postsTitle: 'Alina', postMessage: `i'm learning Html!`, likesCount: 10},
-            {id: v1(), postsTitle: 'Sasha', postMessage: `i'm learning JS!`, likesCount: 10},
-            {id: v1(), postsTitle: 'Andrey', postMessage: `i'm learning TypeScript!`, likesCount: 10},
+            {id: v1(), postMessage: `i'm learning React!`, likesCount: 10},
+            {id: v1(), postMessage: `i'm learning Redux!`, likesCount: 10},
+            {id: v1(), postMessage: `i'm learning Html!`, likesCount: 10},
+            {id: v1(), postMessage: `i'm learning JS!`, likesCount: 10},
+            {id: v1(), postMessage: `i'm learning TypeScript!`, likesCount: 10},
         ],
     },
     dialogsData: {
@@ -54,3 +75,21 @@ export const state = {
 
 }
 
+
+export let addPost = (postMessage: string) => {
+    let newPost: PostType = {
+        id: v1(),
+        postMessage: postMessage,
+        likesCount: 0
+    }
+    state.postsData.post.unshift(newPost)
+    renderEntireTree(state)
+
+    // return {
+    //     ...state,
+    //     postsData: {...state.postsData},
+    //     post: [newPost, ...state.postsData.post]
+    // }
+
+
+}
