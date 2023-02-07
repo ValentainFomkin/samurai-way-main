61bd5d4da18ff6b5e0f345b53f661175e482bb5e
ValentainFomkin <valentain1625@mail.ru>
2023-02-07 20:26:57 +0300

    45 - React JS практика - connect, mapStateToProps, mapDispatchToProps

--- src/components/profile/myPosts/MyPostsContainer.tsx
+++ src/components/profile/myPosts/MyPostsContainer.tsx
@@ -1,31 +1,60 @@
 import React from 'react';
-import {addPostAC, PostDataType, updateNewPostTextAC,} from "../../../redux/profile-reducer";
-import {useDispatch, useSelector} from "react-redux";
-import {ReducersRootType} from "../../../redux/redux-store";
+import {connect} from "react-redux";
 import {MyPosts} from "./MyPosts";
+import {ReducersRootType} from "../../../redux/redux-store";
+import {Dispatch} from "redux";
+import {addPostAC, PostType, updateNewPostTextAC} from "../../../redux/profile-reducer";
 
 
 export type MyPostsPropsType = {}
 
-export const MyPostsContainer: React.FC<MyPostsPropsType> = (props) => {
-    let dispatch = useDispatch()
-    const postData = useSelector<ReducersRootType, PostDataType>(state => state.profileReducer)
+// export const MyPostsContainer: React.FC<MyPostsPropsType> = (props) => {
+//     let dispatch = useDispatch()
+//     const postData = useSelector<ReducersRootType, PostDataType>(state => state.profileReducer)
+//
+//     const addPostHandler = () => {
+//         dispatch(addPostAC())
+//     }
+//
+//     const onPostChange = (text: string) => {
+//         dispatch(updateNewPostTextAC(text))
+//     }
+//
+//     return (
+//         <div>
+//             <MyPosts newPostText={postData.newPostText}
+//                      post={postData.post}
+//                      addPost={addPostHandler}
+//                      updateNewPostText={onPostChange}/>
+//         </div>
+//     );
+// };
 
-    const addPostHandler = () => {
-        dispatch(addPostAC())
+
+export type MapStatePropsType = {
+    post: PostType[]
+    newPostText: string
+}
+export type MapDispatchPropsType = {
+    updateNewPostText: (text: string) => void
+    addPost: () => void
+}
+
+let mapStateToProps = (state: ReducersRootType): MapStatePropsType => {
+    return {
+        newPostText: state.profileReducer.newPostText,
+        post: state.profileReducer.post
     }
-
-    const onPostChange = (text: string) => {
-        dispatch(updateNewPostTextAC(text))
+}
+let mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
+    return {
+        updateNewPostText: (text: string) => {
+            dispatch(updateNewPostTextAC(text))
+        },
+        addPost: () => {
+            dispatch(addPostAC())
+        },
     }
+}
 
-    return (
-        <div>
-            <MyPosts newPostText={postData.newPostText}
-                     post={postData.post}
-                     addPost={addPostHandler}
-                     updateNewPostText={onPostChange}/>
-        </div>
-    );
-};
-
+export const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts)
