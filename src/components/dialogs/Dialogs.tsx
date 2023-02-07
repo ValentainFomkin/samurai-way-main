b08c4f3dbe4f1689c093a2b533b981bb834953f5
ValentainFomkin <valentain1625@mail.ru>
2023-02-07 18:53:46 +0300

    43 React JS Практика - container component (контейнерная компонента)

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,27 +3,31 @@ import s from './Dialogs.module.css'
 import {Dialog} from "./dialog/Dialog";
 import {Message} from "./message/Message";
 import {v1} from "uuid";
-import {AllActionType, DialogsDataType,} from "../../redux/store";
-import {addNewMessageAC, updateNewMessagesTextAC} from "../../redux/dialogs-reducer";
+import {DialogsType, MessagesType} from "../../redux/dialogs-reducer";
 
 export type DialogsPropsType = {
-    dialogsData: DialogsDataType
-    dispatch: (action: AllActionType) => void
-
+    dialogs: DialogsType[]
+    messages: MessagesType[]
+    newMessagesText: string
+    addNewMessage: () => void
+    onChangeTextArea: (text: string) => void
 }
 
 export const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
-    let dialogElements = props.dialogsData.dialogs.map((e, index) => <Dialog key={e.id} id={v1()} name={e.name}/>)
-    let messageElements = props.dialogsData.messages.map((e, index) => <Message key={e.id} id={e.id} text={e.message}/>)
+
+    let dialogElements = props.dialogs.map((e, index) => <Dialog key={e.id} id={v1()} name={e.name}/>)
+    let messageElements = props.messages.map((e, index) => <Message key={e.id} id={e.id} text={e.message}/>)
 
     const addNewMessageHandler = () => {
-        props.dispatch(addNewMessageAC())
+        props.addNewMessage()
+        // dispatch(addNewMessageAC())
     }
 
     const onChangeTextAreaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
         let text = e.currentTarget.value
-        props.dispatch(updateNewMessagesTextAC(text))
+        props.onChangeTextArea(text)
+        // dispatch(updateNewMessagesTextAC(text))
     }
 
     return (
@@ -34,7 +38,7 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
             </div>
             <div className={s.textareaAndButtonDialogs}>
                 <div className={s.dialogsTextArea}>
-                    <textarea value={props.dialogsData.newMessagesText}
+                    <textarea value={props.newMessagesText}
                               onChange={onChangeTextAreaHandler}
                               placeholder='text'/>
                 </div>
