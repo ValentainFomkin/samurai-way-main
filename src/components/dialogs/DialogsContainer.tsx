fc8d3b33b9201cf3d390365b1a40fd54d68209b8
ValentainFomkin <valentain1625@mail.ru>
2023-02-07 20:26:49 +0300

    45 - React JS практика - connect, mapStateToProps, mapDispatchToProps

--- src/components/dialogs/DialogsContainer.tsx
+++ src/components/dialogs/DialogsContainer.tsx
@@ -1,34 +1,67 @@
 import React from 'react';
-import {addNewMessageAC, DialogsDataType, updateNewMessagesTextAC} from "../../redux/dialogs-reducer";
-import {useDispatch, useSelector} from "react-redux";
-import {ReducersRootType} from "../../redux/redux-store";
+import {addNewMessageAC, DialogsType, MessagesType, updateNewMessagesTextAC} from "../../redux/dialogs-reducer";
+import {connect} from "react-redux";
 import {Dialogs} from "./Dialogs";
-import s from "./Dialogs.module.css";
+import {ReducersRootType} from "../../redux/redux-store";
+import {Dispatch} from "redux";
 
 export type DialogsPropsType = {}
 
-export const DialogsContainer: React.FC<DialogsPropsType> = (props) => {
-    let dispatch = useDispatch()
-    const dialogData = useSelector<ReducersRootType, DialogsDataType>(state => state.dialogsReducer)
+// export const DialogsContainer: React.FC<DialogsPropsType> = (props) => {
+//     let dispatch = useDispatch()
+//     const dialogData = useSelector<ReducersRootType, DialogsDataType>(state => state.dialogsReducer)
+//
+//
+//     const addNewMessage = () => {
+//         dispatch(addNewMessageAC())
+//     }
+//
+//     const onChangeTextArea = (text: string) => {
+//         dispatch(updateNewMessagesTextAC(text))
+//     }
+//
+//     return (
+//         <div className={s.dialogs}>
+//             <Dialogs dialogs={dialogData.dialogs}
+//                      newMessagesText={dialogData.newMessagesText}
+//                      messages={dialogData.messages}
+//                      addNewMessage={addNewMessage}
+//                      onChangeTextArea={onChangeTextArea}
+//             />
+//         </div>
+//     );
+// };
 
+type MapStatePropsType = {
+    dialogs: DialogsType[]
+    messages: MessagesType[]
+    newMessagesText: string
+}
+type MapDispatchPropsType = {
+    onChangeTextArea: (text: string) => void
+    addNewMessage: () => void
 
-    const addNewMessage = () => {
-        dispatch(addNewMessageAC())
+}
+
+let mapStateToProps = (state: ReducersRootType): MapStatePropsType => {
+
+    return {
+        dialogs: state.dialogsReducer.dialogs,
+        messages: state.dialogsReducer.messages,
+        newMessagesText: state.dialogsReducer.newMessagesText
     }
+}
+let mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
 
-    const onChangeTextArea = (text: string) => {
-        dispatch(updateNewMessagesTextAC(text))
+    return {
+        onChangeTextArea: (text: string) => {
+            dispatch(updateNewMessagesTextAC(text))
+
+        },
+        addNewMessage: () => {
+            dispatch(addNewMessageAC())
+        },
     }
+}
 
-    return (
-        <div className={s.dialogs}>
-            <Dialogs dialogs={dialogData.dialogs}
-                     newMessagesText={dialogData.newMessagesText}
-                     messages={dialogData.messages}
-                     addNewMessage={addNewMessage}
-                     onChangeTextArea={onChangeTextArea}
-            />
-        </div>
-    );
-};
-
+export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
