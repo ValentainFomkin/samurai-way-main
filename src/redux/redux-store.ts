f5a0e50c7dba205eab1b8a80d71c04c6e1a901b2
ValentainFomkin <valentain1625@mail.ru>
2023-02-10 16:50:12 +0300

    49 - Практика - страница порльзователей

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from "redux";
 import {ProfileReducer} from "./profile-reducer";
 import {DialogsReducer} from "./dialogs-reducer";
 import {SiteBarReducer} from "./siteBar-reducer";
+import {UsersReducer} from "./users-reducer";
 
 let reducers = combineReducers({
     profileReducer: ProfileReducer,
     dialogsReducer: DialogsReducer,
-    siteBarReducer: SiteBarReducer
+    siteBarReducer: SiteBarReducer,
+    usersReducer: UsersReducer,
 })
 
 export type ReducersRootType = ReturnType<typeof reducers>
