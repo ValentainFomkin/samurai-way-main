94ac4e28855aaa74a50b4510c2579c94a3577ea5
ValentainFomkin <valentain1625@mail.ru>
2023-02-10 19:02:30 +0300

    map last friends

--- src/components/friends/friend/Friend.tsx
+++ src/components/friends/friend/Friend.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import s from './Friend.module.css'
+import {Link} from "react-router-dom";
+import {UsersType} from "../../../redux/users-reducer";
 
 export type LastFriendItemsPropsType = {
-    lastNameFriend: string
+    users: UsersType[]
 }
 
 export const Friend: React.FC<LastFriendItemsPropsType> = (props) => {
-    const srcAvatar = "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-617.jpg?w=2000"
 
 
     return (
         <>
-            <div className={s.lastTalkFriend}>
-                <div className={s.avatarFriend}>
-                    <img src={srcAvatar}/>
-                </div>
-                <div className={s.nameFriend}>
-                        <span>
-                            {props.lastNameFriend}
-                        </span>
-                </div>
-            </div>
+            {
+                props.users.map(u => {
+                    return (
+                        <div className={s.lastTalkFriend}>
+                            <div className={s.avatarFriend}>
+                                <img src={u.photos.large}/>
+                            </div>
+                            <div className={s.nameFriend}>
+                                <Link to={`/friends/${u.id}`}>
+                                    {u.name}
+                                </Link>
+                            </div>
+                        </div>
+
+                    )
+                })}
         </>
+
     )
 }
 
