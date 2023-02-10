0384cd8500c45e01a56166e869e2a476d921768f
ValentainFomkin <valentain1625@mail.ru>
2023-02-10 16:50:36 +0300

    49 - Практика - страница порльзователей

---
+++ src/components/users/UsersContainer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {connect} from "react-redux";
+import {User} from "./user/User";
+import {ReducersRootType} from "../../redux/redux-store";
+import {Dispatch} from "redux";
+import {followAC, setUsersAC, unFollowAC, UsersType} from "../../redux/users-reducer";
+
+
+type MapStatePropsType = {
+    users: UsersType[]
+}
+type MapDispatchPropsType = {
+    follow: (userId: string) => void
+    unFollow: (userId: string) => void
+    setUsers: (users: UsersType[]) => void
+}
+
+const mapStateToProps = (state: ReducersRootType): MapStatePropsType => {
+    return {
+        users: state.usersReducer.users
+    }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId))
+        },
+        unFollow: (userId) => {
+            dispatch(unFollowAC(userId))
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users))
+        }
+    }
+}
+
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(User)
