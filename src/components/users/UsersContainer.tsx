f6f45e47b248dbbe5a63fbacc58e43710904fe27
ValentainFomkin <valentain1625@mail.ru>
2023-02-21 16:04:48 +0300

    53 - React JS практика - классовый компонент (extends React.Component)

--- src/components/users/UsersContainer.tsx
+++ src/components/users/UsersContainer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {User} from "./user/User";
 import {ReducersRootType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
 import {followAC, setUsersAC, unFollowAC, UsersType} from "../../redux/users-reducer";
+import {User} from "./user/User";
 
 
 type MapStatePropsType = {
--- src/components/users/user/User.tsx
+++ src/components/users/user/User.tsx
@@ -1,17 +1,16 @@
-import React, {memo, useEffect} from 'react';
-import {UsersType} from "../../../redux/users-reducer";
-import s from './User.module.css'
+import React from 'react';
+import s from "./User.module.css";
 import axios from "axios";
-import userPhoto from '../../../assets/images/3135715.png'
+import {UsersType} from "../../../redux/users-reducer";
+import userPhoto from "../../../assets/images/3135715.png";
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'fa7db0d9-edf1-4a95-a40c-c2d8230fb313'
+        'API-KEY': '22e0b0ed-963e-4cfa-ab70-768eda585bb6'
     }
 })
-
 export type UserPropsType = {
     users: UsersType[]
     follow: (userId: number) => void
@@ -19,72 +18,76 @@ export type UserPropsType = {
     setUsers: (users: UsersType[]) => void
 }
 
-export const User = memo((props: UserPropsType) => {
 
-    const srcAvatarLast = userPhoto
+export class User extends React.Component<UserPropsType> {
 
-    useEffect(() => {
-        instance.get('/users')
-            .then(res => {
-                if (props.users.length <= 4) {
-                    props.setUsers(res.data.items)
-                }
-            })
-    }, [])
+    getUsers = () => {
+        if (this.props.users.length <= 4) {
+            instance.get('/users')
+                .then(res => {
+                    this.props.setUsers(res.data.items)
+                })
+        }
+    }
 
+    render() {
+        return (
+            <div className={s.userContainer}>
 
-    return (
-        <div className={s.userContainer}>
+                <button onClick={this.getUsers}>Get Users</button>
 
-            {
-                props.users.map(u => {
-                        const followOnClickHandler = () => {
-                            if (u.followed) {
-                                props.unFollow(u.id)
-                            } else {
-                                props.follow(u.id)
+                {
+                    this.props.users.map(u => {
+                            const followOnClickHandler = () => {
+                                if (u.followed) {
+                                    this.props.unFollow(u.id)
+                                } else {
+                                    this.props.follow(u.id)
+                                }
                             }
+                            return (
+                                <div key={u.id} className={s.userBlock}>
+                                    <div className={s.imageAndButton}>
+                                        <div className={s.image}>
+                                            <img src={u.photos.large != null ? u.photos.large : userPhoto}/>
+                                        </div>
+                                        <div className={s.buttonFollow}>
+                                            <button onClick={followOnClickHandler}>
+                                                {u.followed ? 'Follow' : 'Unfollow'}
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div className={s.infoProfile}>
+                                        <div className={s.nameAndText}>
+                                            <div className={s.name}>
+                                                <span>{u.name}</span>
+                                            </div>
+                                            <div className={s.text}>
+                                                {u.status}
+                                            </div>
+                                        </div>
+
+                                        <div className={s.countryAndCity}>
+                                            <div className={s.city}>
+                                                {'u.location.city'}
+                                            </div>
+                                            <div className={s.country}>
+                                                {'u.location.country'}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
                         }
-                        return (
-                            <div key={u.id} className={s.userBlock}>
-                                <div className={s.imageAndButton}>
-                                    <div className={s.image}>
-                                        <img src={u.photos.large != null ? u.photos.large : srcAvatarLast}/>
-                                    </div>
-                                    <div className={s.buttonFollow}>
-                                        <button onClick={followOnClickHandler}>
-                                            {u.followed ? 'Follow' : 'Unfollow'}
-                                        </button>
-                                    </div>
-                                </div>
+                    )
+                }
 
-                                <div className={s.infoProfile}>
-                                    <div className={s.nameAndText}>
-                                        <div className={s.name}>
-                                            <span>{u.name}</span>
-                                        </div>
-                                        <div className={s.text}>
-                                            {u.status}
-                                        </div>
-                                    </div>
+            </div>
 
-                                    <div className={s.countryAndCity}>
-                                        <div className={s.city}>
-                                            {'u.location.city'}
-                                        </div>
-                                        <div className={s.country}>
-                                            {'u.location.country'}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                )
-            }
+        );
+    }
+}
 
-        </div>
 
-    );
-});
 
