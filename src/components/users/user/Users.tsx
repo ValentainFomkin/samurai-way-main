42e9ca9bb436ed29eb282b76530a44fc9ca9b6de
ValentainFomkin <valentain1625@mail.ru>
2023-02-22 19:22:17 +0300

    57 - React JS - пример Preloader, loader-gif, isFetching indication

--- src/components/users/user/Users.tsx
+++ src/components/users/user/Users.tsx
@@ -34,67 +34,68 @@ export const Users: React.FC<UserPropsType> = (props) => {
     }
 
 
-    return (
-        <div className={s.userContainer}>
-            {pages.map(p => {
+    return (<>
+
+            <div className={s.userContainer}>
+                {pages.map(p => {
 
 
-                return (
-                    <button key={p}
-                            onClick={() => props.onPageChanged(p)}
-                            className={props.currentPage === p ? s.selectedPage : ''}>{p}</button>
-                )
-            })}
-            {
-                props.users.map(u => {
-                        const followOnClickHandler = () => {
-                            if (u.followed) {
-                                props.unFollow(u.id)
-                            } else {
-                                props.follow(u.id)
+                    return (
+                        <button key={p}
+                                onClick={() => props.onPageChanged(p)}
+                                className={props.currentPage === p ? s.selectedPage : ''}>{p}</button>
+                    )
+                })}
+                {
+                    props.users.map(u => {
+                            const followOnClickHandler = () => {
+                                if (u.followed) {
+                                    props.unFollow(u.id)
+                                } else {
+                                    props.follow(u.id)
+                                }
                             }
+                            return (
+                                <div key={u.id} className={s.userBlock}>
+                                    <div className={s.imageAndButton}>
+                                        <div className={s.image}>
+                                            <img src={u.photos.large != null ? u.photos.large : userPhoto}/>
+                                        </div>
+                                        <div className={s.buttonFollow}>
+                                            <button onClick={followOnClickHandler}>
+                                                {u.followed ? 'Follow' : 'Unfollow'}
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div className={s.infoProfile}>
+                                        <div className={s.nameAndText}>
+                                            <div className={s.name}>
+                                                <span>{u.name}</span>
+
+                                            </div>
+                                            <div className={s.text}>
+                                                {u.status}
+                                            </div>
+                                        </div>
+
+                                        <div className={s.countryAndCity}>
+                                            <div className={s.city}>
+                                                {'u.location.city'}
+                                            </div>
+                                            <div className={s.country}>
+                                                {'u.location.country'}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
                         }
-                        return (
-                            <div key={u.id} className={s.userBlock}>
-                                <div className={s.imageAndButton}>
-                                    <div className={s.image}>
-                                        <img src={u.photos.large != null ? u.photos.large : userPhoto}/>
-                                    </div>
-                                    <div className={s.buttonFollow}>
-                                        <button onClick={followOnClickHandler}>
-                                            {u.followed ? 'Follow' : 'Unfollow'}
-                                        </button>
-                                    </div>
-                                </div>
+                    )
+                }
 
-                                <div className={s.infoProfile}>
-                                    <div className={s.nameAndText}>
-                                        <div className={s.name}>
-                                            <span>{u.name}</span>
-
-                                        </div>
-                                        <div className={s.text}>
-                                            {u.status}
-                                        </div>
-                                    </div>
-
-                                    <div className={s.countryAndCity}>
-                                        <div className={s.city}>
-                                            {'u.location.city'}
-                                        </div>
-                                        <div className={s.country}>
-                                            {'u.location.country'}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                )
-            }
-
-        </div>
-
+            </div>
+        </>
     );
 
 
