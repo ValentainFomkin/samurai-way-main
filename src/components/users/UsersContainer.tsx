36df03fd5b7864c30dbc8a6742085f997602233b
ValentainFomkin <valentain1625@mail.ru>
2023-02-22 16:22:40 +0300

    55 - React JS практика - pagination, постраничный вывод пользователей

--- src/components/users/UsersContainer.tsx
+++ src/components/users/UsersContainer.tsx
@@ -2,22 +2,38 @@ import React from 'react';
 import {connect} from "react-redux";
 import {ReducersRootType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
-import {followAC, setUsersAC, unFollowAC, UsersType} from "../../redux/users-reducer";
+import {
+    followAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unFollowAC,
+    UsersType
+} from "../../redux/users-reducer";
 import {User} from "./user/User";
 
 
 type MapStatePropsType = {
     users: UsersType[]
+    pageSize: number
+    totalUserCount: number
+    currentPage: number
+
 }
 type MapDispatchPropsType = {
     follow: (userId: number) => void
     unFollow: (userId: number) => void
     setUsers: (users: UsersType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalUsersCountAC: (currentTotalCount: number) => void
 }
 
 const mapStateToProps = (state: ReducersRootType): MapStatePropsType => {
     return {
-        users: state.usersReducer.users
+        users: state.usersReducer.users,
+        pageSize: state.usersReducer.pageSize,
+        totalUserCount: state.usersReducer.totalUserCount,
+        currentPage: state.usersReducer.currentPage
     }
 }
 
@@ -31,6 +47,12 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (currentPage: number) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCountAC: (currentTotalCount: number) => {
+            dispatch(setTotalUsersCountAC(currentTotalCount))
         }
     }
 }
